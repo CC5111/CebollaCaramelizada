34a607e42a7ff207b2f6171d0da85962f605754b
Manuel Cáceres <el.ariel.cl@gmail.com>
2016-08-13 17:24:47 -0400

    Proving DAOS

--- app/models/entities/LibrarySeries.scala
+++ app/models/entities/LibrarySeries.scala
@@ -1,11 +1,11 @@
 package models.entities
 
-case class Series(id: Long, title: String, description: String, seasonNumber : Int, state: String, image: String)
+case class Series(id: Long, title: String, description: String, seasonNumber : Int, state: String, image: String)  extends BaseEntity
 
-case class Season(id: Long, seriesID: Long, number:Int, title:String, description: String, epsNumber : Int, state: String, image: String)
+case class Season(id: Long, seriesID: Long, number:Int, title:String, description: String, epsNumber : Int, state: String, image: String) extends BaseEntity
 
-case class Episode(id: Long, seasonID: Long, number:Int, title:String, description: String, duration : Int, state: String, image: String, language:String, requested:Boolean, source:String, progress:Double)
+case class Episode(id: Long, seasonID: Long, number:Int, title:String, description: String, duration : Int, state: String, image: String, language:String, requested:Boolean, source:String, progress:Double)  extends BaseEntity
 
-case class Subtitle(id: Long, episodeID: Long, language:String, source:String)
+case class Subtitle(id: Long, episodeID: Long, language:String, source:String)  extends BaseEntity
 
 
