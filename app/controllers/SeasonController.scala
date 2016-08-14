2b48c3443804c85f20f83ee4a2c87437804b4331
Manuel Cáceres <el.ariel.cl@gmail.com>
2016-08-14 04:04:03 -0300

    Evoluciones de creacion de tabla y data inicial

--- app/controllers/SeasonController.scala
+++ app/controllers/SeasonController.scala
@@ -5,8 +5,8 @@ import play.api._
 import play.api.mvc._
 
 @Singleton
-class TemporadaController @Inject() extends Controller {
-  def show(id: Long) = Action {
+class SeasonController @Inject() extends Controller {
+  def show(idSeries: Long, idSeason: Long) = Action {
     /* TODO this should search a Temporada on the DB by an ID */
     Ok(views.html.temporada("Temporada 1"))
   }
